3c2794ce667ffa18ad6a743ea45ce3df62cfbfe4
Savchenko <savchenko.ra@yandex.ru>
2023-09-27 16:36:52 +0300

    done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] originalArray = {1, 2, 3, 4, 5};
+        int[] reversedArray = reverse(originalArray);
+
+        System.out.println("Original Array: " + Arrays.toString(originalArray));
+        System.out.println("Reversed Array: " + Arrays.toString(reversedArray));
+    }
+
+    public static int[] reverse(int[] array) {
+        int length = array.length;
+        int[] reversed = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            reversed[i] = array[length - 1 - i];
+        }
+
+        return reversed;
     }
 }
